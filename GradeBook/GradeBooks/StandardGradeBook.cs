ee71ebf920d9963431a2b3ca86fc776c5162ed42
Aurel Ioan <aurelioan93@gmail.com>
2020-07-30 13:47:00 +0200

    Update StandardGradeBook.cs

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -5,9 +5,9 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    public class StandardGradebook : BaseGradeBook
+    public class StandardGradeBook : BaseGradeBook
     {
-        public StandardGradebook(string name) : base(name)
+        public StandardGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Standard;
         }
